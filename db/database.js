67ec63d8ed6cf252268cd3dde052265afef3fb96
Felipe Hernández Palafox <felipe.sistemas.fhp@gmail.com>
2021-05-05 18:20:32 -0500

    Update database.js

--- db/database.js
+++ db/database.js
@@ -1,20 +1,45 @@
-const mysql = require('mysql');
+var mysql =  require("mysql");
 
-const mysqlConnection = mysql.createConnection({
-  host: 'us-cdbr-east-03.cleardb.com',
-  user: 'b4900007e06841',
-  password: 'dea9cb74',
-  database: 'heroku_9c613d96d271cbe',
-  multipleStatements: true
+
+var pool = mysql.createPool({
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'b4900007e06841',
+    password: 'dea9cb74',
+    database: 'heroku_9c613d96d271cbe',
 });
 
-mysqlConnection.connect(function (err) {
-  if (err) {
-    console.error(err);
-    return;
-  } else {
-    console.log('db is connected');
-  }
-});
+modeDevelopper  =  function(){
+    console.log(process.env.NODE_ENV);
+    if ( process.env.NODE_ENV === 'test' ) { 
+        return true;  
+    }
+    return false;
+}
 
-module.exports = mysqlConnection;
+module.exports = {
+       query: function(){
+           var sql_args = [];
+           var args = [];
+           for(var i=0; i<arguments.length; i++){
+               args.push(arguments[i]);
+           }
+           var callback = args[args.length-1]; //last arg is callback
+           pool.getConnection(function(err, connection) {
+           if(err) {
+                   console.log(err);
+                   return callback(err);
+               }
+               if(args.length > 2){
+                   sql_args = args[1];
+               }
+           connection.query(args[0], sql_args, function(err, results) {
+             connection.release(); // always put connection back in pool after last query
+             if(err){
+                       console.log(err);
+                       return callback(err);
+                   }
+             callback(null, results);
+           });
+         });
+       }
+};
